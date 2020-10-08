ae0139e50fcc46977dcbe0df715947144d147d50
RICARDO ALMEIDA DA ROCHA <ricardoalmeida.for@gmail.com>
2020-10-08 10:38:57 -0300

    Criação de Components Filme/Autor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FilmeComponent } from './filmes/filme.component'
+import { AutorComponent } from './autor/autor.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilmeComponent,
+    AutorComponent
   ],
   imports: [
     BrowserModule,
